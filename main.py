5d68d07f7bd577f433d434627fb8203aa65af81a
Chyngyz <tagaev008ch@gmail.com>
2023-10-13 18:33:32 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+# file = open('test.html',encoding='utf-8')
+
+# html = file.read()
+
+# soup = BS(html,'html.parser')
+# menu_list = soup.find("div",{"class":"container"})
+# nav_menu = menu_list.find("div",{"class":"navigator-container"})
+# ul = nav_menu.find("ul",{"class":"menu"})
+# li_list = ul.find_all("li")
+
+# for li in li_list:
+#     # print(li.text.strip())
+
+# content = soup.find("div",{"class":"container"})
+# cont_container = content.find("div",{"class":"content-container"})
+# post = cont_container.find("div",{"class":"post"})
+# post_list = cont_container.find_all("div",{"class":"post"})
+
+# for po in post_list:
+#     print(po.text.strip())
+
+# content = soup.find("div",{"class":"container"})
+# footer = content.find("div",{"class":"footer"})
+# box = footer.find_all("div",{"class":"box"})
+# for i in box:
+#     content =  i.find("div",{"class" : "box-content"})
+#     p = content.find("p",{"class": "description"})
+#     print(p.text.strip())
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main2.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+# URL = "https://www.house.kg/snyat-kvartiru?" 
+# def get_html(url):
+#     respons = requests.get(url)
+#     if respons.status_code == 200:
+#         return respons.txt
+#     return None
+
+
+# def main():
+#     URL = "https://www.house.kg/snyat-kvartiru?" 
+#     html = get_html(URL)
